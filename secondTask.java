bf951a3350a8dc4201d715c6f69f159494e04808
sivaranja2074 <105847429+sivaranja2074@users.noreply.github.com>
2022-05-19 01:50:19 -0700

    Add files via upload

---
+++ secondTask.java
@@ -0,0 +1,300 @@
+package myTask;
+
+import java.util.Scanner;
+
+class Stringmethods{
+	String string;
+	int strlength ;
+	int index;
+	char[] strArray ; 
+	Stringmethods(String string){
+		this.string=string;
+		this.strArray=string.toCharArray();
+		int strArray_length =strArray.length;
+		this.strlength=strArray_length;
+	}
+	public char charAt(int index) {
+			if(index<strlength)
+				return strArray[index];
+			return '\0';
+		}	
+  public int codePointAt(int index) {
+	  
+		if(index<strlength) {
+		int asciivalue=strArray[index];
+		return asciivalue;
+		}
+		return '\0';
+     }
+  
+public int codePointBefore(int index) {
+	if(index<strlength) {
+		int asciivalue=strArray[index-1];
+		return asciivalue;
+		}
+		return '\0';
+     }
+public int codePointCount(int startindex,int endindex) {
+	  
+
+    int count=0;
+           if(endindex>strArray.length) {
+		    return '\0';
+	      }
+    		for(int i=startindex;i<endindex;i++) {
+    			count++;
+    		 }
+    		return count;
+ }
+  
+  public String concat(String concat_string) {
+	  return string+concat_string;
+  }
+  
+  public boolean contains(String otherstr) {
+	  
+	boolean contain_string=this.string.matches("(.*)"+otherstr+"(.*)");
+	return contain_string;
+	  
+  }
+  
+  public boolean  contentEquals(String otherstr) {
+	char[] otherstr_Array=otherstr.toCharArray();
+      for (int i = 0; i < this.string.length(); i++){
+   
+             if (this.strArray[i] != otherstr_Array[i]) {
+            	  return false;
+             }
+          
+      }
+ 
+       return true;
+  }
+  
+public int compareTo(String otherstr) {
+	
+	char[] otherstr_Array=otherstr.toCharArray();	
+	int otherstr_length=otherstr_Array.length;
+	
+		for(int i=0;i<this.strlength;i++) {
+		
+			 if(this.strArray[i] != otherstr_Array[i]){
+				 int asciivalue_str=this.strArray[i];
+				 int asciivalue_otherstr=otherstr_Array[i];
+				 return asciivalue_str-asciivalue_otherstr;
+			 }
+			 if(this.strlength!=otherstr_length) {
+				 return this.strlength-otherstr_length;
+			 }
+		}
+		return 0;
+	
+	
+	
+}
+public int compareToIgnoreCase(String otherstr) {
+	
+	char[] otherstr_Array=otherstr.toCharArray();	
+	int otherstr_length=otherstr_Array.length;
+	
+		for(int i=0;i<this.strlength;i++) {
+			 int asciivalue_str=this.strArray[i];
+			 int asciivalue_otherstr=otherstr_Array[i];
+			 if(asciivalue_str-asciivalue_otherstr==32||asciivalue_str-asciivalue_otherstr==-32) {
+					continue;
+			 }
+			 if(this.strArray[i] != otherstr_Array[i]){
+			
+				 return asciivalue_str-asciivalue_otherstr;
+			 }
+			 if(this.strlength!=otherstr_length) {
+				 return this.strlength-otherstr_length;
+			 }
+			 
+
+		}
+		return 0;
+}
+
+public String toString() { //stringArray to string
+	   String covert_string="";	   
+	  for(int i=0;i<this.strlength;i++) 
+	  {
+		  covert_string+=this.strArray[i]; 
+	  }
+	return covert_string;
+	 }
+
+
+public String toUpperCase() {
+	String upper_string="";
+	for(int i=0;i<this.strlength;i++) {
+		int char_value=this.strArray[i];
+	      if(char_value>=97&&char_value<=122) {
+	    	  char_value=char_value-32;
+	    	  upper_string+=(char)char_value;
+	    	 
+	      } else {
+	    	  upper_string+=strArray[i];
+	      }
+	}
+	return upper_string;
+}
+public String toLowerCase() {
+	String lower_string="";
+	for(int i=0;i<this.strlength;i++) {
+		int char_value=this.strArray[i];
+	      if(char_value>=65&&char_value<=90) {
+	    	  char_value=char_value+32;
+	    	  lower_string+=(char)char_value;
+	    	 
+	      } else {
+	    	  lower_string+=strArray[i];
+	      }
+	}
+	return lower_string;
+}
+public boolean  equals(String otherstr) {
+	char[] otherstr_Array=otherstr.toCharArray();
+     for (int i = 0; i < this.string.length(); i++){
+            if (this.strArray[i] != otherstr_Array[i]) {
+           	  return false;
+            }
+         
+     }
+
+      return true;
+ }
+
+public boolean equalsIgnoreCase(String otherstr) {
+	
+	char[] otherstr_Array=otherstr.toCharArray();	
+	int otherstr_length=otherstr_Array.length;
+	
+		for(int i=0;i<this.strlength;i++) {
+			 int asciivalue_str=this.strArray[i];
+			 int asciivalue_otherstr=otherstr_Array[i];
+			 if(asciivalue_str-asciivalue_otherstr==32||asciivalue_str-asciivalue_otherstr==-32) {
+					continue;
+			 }
+			 if(this.strArray[i] != otherstr_Array[i]){
+			
+				 return false;
+			 }
+			
+		}
+		return true;
+}
+
+public boolean isEmpty() {
+	if(this.strlength==0) {
+		return true;
+	}
+	return false;
+}
+
+public String replace(char oldChar,char newChar) {
+	String replacedString="";
+	  for (int i = 0; i < this.string.length(); i++){
+	      if (this.strArray[i] == oldChar) {
+	    	  replacedString+=newChar;
+	      }
+	      replacedString+=strArray[i]; 
+	}
+	return replacedString;
+}
+
+public int indexOf(char character) {
+	  for (int i = 0; i < this.string.length(); i++){
+	      if (this.strArray[i] == character) {
+	   	  return i;
+	      }
+	}
+	  
+	return '\0';
+
+
+	}
+
+
+public boolean endsWith(String otherstr) {
+	char[] otherstr_Array=otherstr.toCharArray();
+	int otherstr_length=otherstr_Array.length;
+    	 if(this.strArray[strlength-1]==otherstr_Array[otherstr_length-1]) {
+    		 return true;
+    	 }
+	return false;
+	
+}
+
+public boolean startsWith(String otherstr) {
+	char[] otherstr_Array=otherstr.toCharArray();
+	int otherstr_length=otherstr_Array.length;
+    	 if(this.strArray[strlength-strlength]==otherstr_Array[otherstr_length-otherstr_length]) {
+    		 return true;
+    	 }
+	return false;
+	
+}
+
+public String valueOf(int value) { //int to string
+	   String covert_string="";	   
+	  for(int i=0;i<value;i++) 
+	  {
+		  covert_string+=value; 
+	  }
+	return covert_string;
+	 }
+public String valueOf(float value) { //float to string
+	   String covert_string="";	   
+	  for(int i=0;i<value;i++) 
+	  {
+		  covert_string+=value; 
+	  }
+	return covert_string;
+	 }
+
+
+
+
+
+
+
+
+
+}
+  
+  
+
+public class secondTask {
+public static void main(String[] args) {
+	
+	System.out.println("Enter the String : ");
+	Scanner scanner=new Scanner(System.in);
+	String str=scanner.nextLine();	
+	Stringmethods obj=new Stringmethods(str);
+	System.out.println( obj.charAt(2));
+	System.out.println(obj.codePointAt(6));
+	System.out.println(obj.codePointBefore(3));
+	System.out.println(obj.codePointCount(1, 5));
+	System.out.println(obj.concat("Krishnakavi"));
+	System.out.println(obj.contains("ajaykavi"));
+	System.out.println(obj.contentEquals("sivaranjakavi"));
+	System.out.println(obj.compareTo("sivaranjakavi"));
+	System.out.println(obj.compareToIgnoreCase("sivaranja"));
+	System.out.println(obj.toUpperCase());
+	System.out.println(obj.toLowerCase());
+	System.out.println(obj.equals("sivaranja"));
+	System.out.println(obj.equalsIgnoreCase("abc"));
+	System.out.println(obj.isEmpty());
+	System.out.println(obj.endsWith("nja"));
+	System.out.println(obj.startsWith("siva"));
+	System.out.println(obj.indexOf('s'));
+	
+	
+	 
+ 
+	
+}
+}
+
